4ead83151f896fafa04ece058f026fad534ecd36
insub4067 <vb15@kakao.com>
2022-11-18 16:21:31 +0900

    init

---
+++ UIKitPractice-GitHubApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
