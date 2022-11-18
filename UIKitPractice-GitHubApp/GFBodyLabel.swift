b5b5e1fa150bb10d0b877c38f6d0fe38c1763c0d
insub4067 <vb15@kakao.com>
2022-11-19 00:45:38 +0900

    feat : GFBodyLabel

---
+++ UIKitPractice-GitHubApp/GFBodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFBodyLabel.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/19.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        font = UIFont.preferredFont(forTextStyle: .body)
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
