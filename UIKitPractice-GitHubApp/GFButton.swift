2ba45bc53fb0b2e395ee7f1302ab3f3957271cde
insub4067 <vb15@kakao.com>
2022-11-18 17:10:27 +0900

    feat : GFButton

---
+++ UIKitPractice-GitHubApp/GFButton.swift
@@ -0,0 +1,32 @@
+//
+//  GFButton.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/18.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
