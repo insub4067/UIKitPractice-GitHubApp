bfc01d05102b74cd916cb4d301d2d4a34be3785b
insub4067 <vb15@kakao.com>
2022-11-19 10:38:07 +0900

    feat : GFAlertVC

--- UIKitPractice-GitHubApp/GFBodyLabel.swift
+++ UIKitPractice-GitHubApp/GFBodyLabel.swift
@@ -18,7 +18,7 @@ class GFBodyLabel: UILabel {
         fatalError("init(coder:) has not been implemented")
     }
 
-    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+    init(textAlignment: NSTextAlignment) {
         super.init(frame: .zero)
         self.textAlignment = textAlignment
         configure()
---
+++ UIKitPractice-GitHubApp/UIViewController+.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController+.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/19.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+}
