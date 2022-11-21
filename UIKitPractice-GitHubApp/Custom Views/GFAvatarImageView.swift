c85e024fc293e0b5440abef382de8d3dad7e6431
insub4067 <vb15@kakao.com>
2022-11-22 07:20:16 +0900

    feat : setCache

--- UIKitPractice-GitHubApp/Custom Views/GFAvatarImageView.swift
+++ UIKitPractice-GitHubApp/Custom Views/GFAvatarImageView.swift
@@ -36,15 +36,15 @@ class GFAvatarImageView: UIImageView {
         }
 
         guard let url = URL(string: urlString) else { return }
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+
             if error != nil { return }
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
             guard let data = data else { return }
             guard let image = UIImage(data: data) else { return }
-            DispatchQueue.main.async { [weak self] in
-                guard let self = self else { return }
-                self.image = image
-            }
+            self.cache.setObject(image, forKey: cacheKey)
+            DispatchQueue.main.async { self.image = image }
         }
         task.resume()
     }
