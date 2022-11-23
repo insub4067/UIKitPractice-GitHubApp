1cd2ec800e6ae3155cd588f9c54d7a3e606dd89b
insub4067 <vb15@kakao.com>
2022-11-23 13:33:44 +0900

    feat : Network - getFollowerInfo

--- UIKitPractice-GitHubApp/Managers/NetworkManager.swift
+++ UIKitPractice-GitHubApp/Managers/NetworkManager.swift
@@ -15,16 +15,6 @@ class NetworkManager {
     private init() {}
 
     func getFollowers(for username: String, page: Int, completion: @escaping (Result<[Follower], GFError>) -> Void) {
-
-        let user = "insub4067"
-        let token = "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"
-        let loginString = String(format: "%@:%@", user, token)
-        let loginData = loginString.data(using: String.Encoding.utf8)
-
-        guard let loginData = loginData else { return }
-
-        let base64LoginString = loginData.base64EncodedString()
-
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
 
         guard let url = URL(string: endpoint) else {
@@ -32,10 +22,7 @@ class NetworkManager {
             return
         }
 
-        var request = URLRequest(url: url)
-        request.addValue(base64LoginString, forHTTPHeaderField: "Authorization")
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let _ = error {
                 completion(.failure(.unableToComplete))
             }
@@ -52,6 +39,38 @@ class NetworkManager {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(.success(followers))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+
+    func getFollowerInfo(for username: String, completion: @escaping (Result<User, GFError>) -> Void) {
+        let endpoint = baseURL + "\(username)"
+
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidUsername))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completion(.failure(.unableToComplete))
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
             } catch {
                 completion(.failure(.invalidData))
             }
