6b862fc5468b491b5e8c8309d3fa1b66c2b922b3
insub4067 <vb15@kakao.com>
2022-11-25 11:38:57 +0900

    feat : GFUserInfoHeaderVC

---
+++ UIKitPractice-GitHubApp/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/25.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
