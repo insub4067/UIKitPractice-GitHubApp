af108fb85e611b9db07a7cfb46c9afd1e7ff43e5
insub4067 <vb15@kakao.com>
2022-11-26 05:36:12 +0900

    refactor : frame -> bounds

--- UIKitPractice-GitHubApp/Screens/UserInfoVC.swift
+++ UIKitPractice-GitHubApp/Screens/UserInfoVC.swift
@@ -50,7 +50,7 @@ class UserInfoVC: UIViewController {
     func add(childVC: UIViewController, to containerView: UIView) {
         addChild(childVC)
         containerView.addSubview(childVC.view)
-        childVC.view.frame = containerView.frame
+        childVC.view.frame = containerView.bounds
         childVC.didMove(toParent: self)
     }
 
