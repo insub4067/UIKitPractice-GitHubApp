0942df421785f4f320a0cfc0911a9522e254a01a
insub4067 <vb15@kakao.com>
2022-11-26 06:28:12 +0900

    feat : GFRepoItemVC

--- UIKitPractice-GitHubApp/Custom Views/ViewControllers/GFItemInfoVC.swift
+++ UIKitPractice-GitHubApp/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -14,6 +14,8 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
 
+    var user: User!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
@@ -21,6 +23,15 @@ class GFItemInfoVC: UIViewController {
         configureStackView()
     }
 
+    init(user: User!) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private func configureBackgroundView() {
         view.layer.cornerRadius = 18
         view.backgroundColor = .secondaryLabel
---
+++ UIKitPractice-GitHubApp/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemVC.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/26.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, with: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, with: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
