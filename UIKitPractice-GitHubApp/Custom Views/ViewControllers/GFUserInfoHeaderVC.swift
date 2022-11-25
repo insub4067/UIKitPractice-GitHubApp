0495dc1c8be1402416a15756e8fe894cd9705ac9
insub4067 <vb15@kakao.com>
2022-11-25 10:54:26 +0900

    feat : GFUserInfoHeaderVC - init

---
+++ UIKitPractice-GitHubApp/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,15 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  UIKitPractice-GitHubApp
+//
+//  Created by Kim Insub on 2022/11/25.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
