25ac729c4339a9a2e73e281aa6080f3db6325648
insub4067 <vb15@kakao.com>
2022-11-27 00:12:16 +0900

    refactor

--- UIKitPractice-GitHubApp/Custom Views/Views/GFEmptyStateView.swift
+++ UIKitPractice-GitHubApp/Custom Views/Views/GFEmptyStateView.swift
@@ -48,12 +48,10 @@ class GFEmptyStateView: UIView {
             messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
             messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
             messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
-//            messageLabel.heightAnchor.constraint(equalToConstant: 200),
 
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
-            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-//            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170)
         ])
     }
 }
