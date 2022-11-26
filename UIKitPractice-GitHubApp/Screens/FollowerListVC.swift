40fe3d8f858b1db76452b74e5ee3746e734889db
insub4067 <vb15@kakao.com>
2022-11-26 16:47:05 +0900

    refactor : Optimize

--- UIKitPractice-GitHubApp/Screens/FollowerListVC.swift
+++ UIKitPractice-GitHubApp/Screens/FollowerListVC.swift
@@ -24,6 +24,16 @@ class FollowerListVC: UIViewController {
     var hasMoreFollower = true
     var isSearching = false
 
+    init(username: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.username = username
+        title = username
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
